987db69765f9e3956242913f74cfb6d57418207f
Avinash <Avinash99@gmail.com>
2025-08-19 11:52:24 +0530

    parameter added

--- hello_world.groovy
+++ hello_world.groovy
@@ -1,5 +1,26 @@
 @Library('liquibase_shared_library')
 import java.text.SimpleDateFormat
+loadEnvVars()
+
+properties([
+    parameters([
+        string(
+            name: 'PROJECT_KEY', 
+            defaultValue: 'avidere', 
+            description: 'Project key for the Liquibase project'
+        ),
+        string(
+            name: 'REPOSITORY_NAME',
+            defaultValue: 'Ansible-Deployment',
+            description: 'Repository name for the Liquibase project'
+        ),
+        string(
+            name: 'BRANCH_NAME', 
+            defaultValue: 'main', 
+            description: 'Branch name to checkout'
+        ),
+    ])
+])
 pipeline {
     agent any
 
